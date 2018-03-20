ca918bcd704509c7b60712b042d3a9a476654038
Bryan Norden <bryan.norden@yahoo.com>
2018-03-20 16:54:49 -0700

    Added method to get chance of winning as a percentage for player A vs player B based on their Elo ratings

--- Sources/EloRating/EloRating.swift
+++ Sources/EloRating/EloRating.swift
@@ -1,3 +1,25 @@
-struct EloRating {
-    var text = "Hello, World!"
+import Foundation
+
+public struct EloRating {
+  
+    /**
+     Chance of Winning for a player vs another player
+     
+     - parameters:
+        - forPlayer: Player rating (Float)
+        - vs: Rating (Float) of player you want to beat
+     
+     - returns:
+     Decimal percentage change of player A beating player B
+     
+     - Note:
+     chanceOfWinning = 1 / (1 + 10^((B - A)/400) )
+    
+     - seealso:
+     [Elo Rating System](https://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details)
+     */
+    func chanceOfWinning(forPlayer ratingA: Float, vs ratingB: Float) -> Float {
+        return 1 / (1 + pow(10,((ratingB - ratingA) / 400.0)))
+    }
+
 }
--- Tests/EloRatingTests/EloRatingTests.swift
+++ Tests/EloRatingTests/EloRatingTests.swift
@@ -2,15 +2,37 @@ import XCTest
 @testable import EloRating
 
 class EloRatingTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(EloRating().text, "Hello, World!")
+    
+    func testChanceOfWinningFor_100_vs_75() {
+        let rating = EloRating().chanceOfWinning(forPlayer: 100.0, vs: 75.0)
+        XCTAssertEqual(rating, 0.535, accuracy: 0.001)
+    }
+    
+    func testChanceOfWinningFor_75_vs_100() {
+        let rating = EloRating().chanceOfWinning(forPlayer: 75.0, vs: 100.0)
+        XCTAssertEqual(rating, 0.464, accuracy: 0.001)
     }
 
-
+    func testChanceOfWinningFor_1_And_10() {
+        let rating = EloRating().chanceOfWinning(forPlayer: 1, vs: 10)
+        XCTAssertEqual(rating, 0.487, accuracy: 0.001)
+    }
+    
+    func testChanceOfWinningFor_10_And_1() {
+        let rating = EloRating().chanceOfWinning(forPlayer: 10, vs: 1)
+        XCTAssertEqual(rating, 0.512, accuracy: 0.001)
+    }
+    
+    func testChanceOfWinning_sameRatings() {
+        let rating = EloRating().chanceOfWinning(forPlayer: 1, vs: 1)
+        XCTAssertEqual(rating, 0.500, accuracy: 0.001)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testChanceOfWinningFor_100_vs_75", testChanceOfWinningFor_100_vs_75),
+        ("testChanceOfWinningFor_75_vs_100", testChanceOfWinningFor_75_vs_100),
+        ("testChanceOfWinningFor_1_And_10", testChanceOfWinningFor_1_And_10),
+        ("testChanceOfWinningFor_10_And_1", testChanceOfWinningFor_10_And_1),
+        ("testChanceOfWinning_sameRatings", testChanceOfWinning_sameRatings),
     ]
 }
